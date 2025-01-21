e69912d921b264b27eb33c8d16293f02914c2d27
palascinochristian <palascinochristian@gmail.com>
2025-01-21 12:57:09 +0100

    feat: add posts array

---
+++ data/posts.js
@@ -0,0 +1,29 @@
+const posts = [
+      {
+        title: "Ciambellone della Nonna",
+        comment: "Un dolce soffice e profumato, perfetto per colazioni e merende indimenticabili!",
+        image: "/imgs/ciambellone.jpeg"
+      },
+      {
+        title: "Cracker alla Barbabietola",
+        comment: "Snack croccante e colorato, ideale per aperitivi o spuntini sani.",
+        image: "/imgs/cracker_barbabietola.jpeg"
+      },
+      {
+        title: "Pane Fritto Dolce",
+        comment: "Una golosa ricetta della tradizione, perfetta per i più golosi!",
+        image: "/imgs/pane_fritto_dolce.jpeg"
+      },
+      {
+        title: "Pasta alla Barbabietola",
+        comment: "Un primo piatto sorprendente, cremoso e ricco di colore.",
+        image: "/imgs/pasta_barbabietola.jpeg"
+      },
+      {
+        title: "Torta Paesana",
+        comment: "Un dolce rustico e genuino, che sa di casa e tradizione.",
+        image: "/imgs/torta_paesana.jpeg"
+      }
+    ];
+
+    module.exports = posts;
